8bbb7107536cb857033fec37cb619bc8402b5607
Irfan Manzoor <rayazl@outlook.com>
2024-03-31 22:07:54 +0500

    variables

---
+++ src/01-Python-101/02_variables.py
@@ -0,0 +1,48 @@
+# Variables : Objects contatining data
+# - String
+st = "hello"
+print (st)
+
+# - Integer
+i = 3
+print (i)
+
+# - Float
+f = 3.14
+print (f)
+
+# - Boolean
+b = True
+print (b)
+
+# - List
+l = [1, 2, 3]
+print (l)
+
+# - Set
+s = {1, 2, 3}
+print (s)
+
+# - Tuple
+t = (1, 2, 3)
+print (t)
+
+# - Dictionary
+d = {
+    "a": 1,
+    "b": 2
+}
+print (d)
+
+# - None
+n = None
+print (n)
+
+# class of variables
+print (type (st), type (i), type (f), type (b), type (l), type (s), type (t), type (d), type (n))
+
+# Rules for Python Variables
+# - A variable name must start with a letter or the underscore character
+# - A variable name cannot start with a number
+# - A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# - Variable names are case-sensitive (age, Age and AGE are three different variables)
