c7e56f39c52a52125bdad224832e1960c578c48b
Irfan Manzoor <rayazl@outlook.com>
2024-03-31 22:08:15 +0500

    arithmatic operators

---
+++ src/01-Python-101/03_operators.py
@@ -0,0 +1,12 @@
+print (2 + 1) # Addition - integer return
+print (2 - 1) # Subtraction - integer return
+print (2 / 1) # Divide - float return
+print (2 // 1) # Division - integer return
+print (2 * 1) # Multiplication - integer return
+print (2 % 1) # Remainder - integer return
+print (2 ** 1) # Exponentiation - integer return
+
+print ((3 ** 2) / 2 * 3 / 3 + 6 - 4)
+
+# Arithmatic Operators Execution Order (Left -> Right sequence)
+# PEMDAS - Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
