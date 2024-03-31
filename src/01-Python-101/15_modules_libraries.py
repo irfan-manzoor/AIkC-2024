76cdac1352d203d24605925ab4920f48d360f96f
Irfan Manzoor <rayazl@outlook.com>
2024-03-31 22:11:54 +0500

    data-science-libraries

---
+++ src/01-Python-101/15_modules_libraries.py
@@ -0,0 +1,18 @@
+# Important Libraries
+# pandas
+# numpy
+# matplotlib
+# seaborn
+# scipy
+# statsmodels
+
+import pandas as pd
+import numpy as np
+
+print (np.sqrt(4))
+
+import math
+print (math.sqrt(4))
+
+from math import sqrt
+print (sqrt(4))
