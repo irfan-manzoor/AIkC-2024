4fd13405e4fc68ebf4d1621e6f4948eb859c6892
Irfan Manzoor <rayazl@outlook.com>
2024-03-31 22:08:42 +0500

    data-types

---
+++ src/01-Python-101/04_data_types.py
@@ -0,0 +1,19 @@
+i = 82 # 'int' type
+a = 84.0 # 'float' type
+b = "hello" # 'str' type
+c = True # 'bool' type
+d = [1, 2, 3] # 'list' type
+e = (1, 2, 3) # 'tuple' type
+f = {1, 2, 3} # 'set' type
+g = {"a": 1, "b": 2} # 'dict' type
+h = None # 'None' type
+
+print (type(i)) # type of i
+print (type(a)) # type of a
+print (type(b)) # type of b
+print (type(c)) # type of c
+print (type(d)) # type of d
+print (type(e)) # type of e
+print (type(f)) # type of f
+print (type(g)) # type of g
+print (type(h)) # type of h
