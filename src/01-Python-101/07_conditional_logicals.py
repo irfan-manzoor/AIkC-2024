839d766e3af48c531f00c063cd25dd9d3305a336
Irfan Manzoor <rayazl@outlook.com>
2024-03-31 22:09:32 +0500

    logical-operators

---
+++ src/01-Python-101/07_conditional_logicals.py
@@ -0,0 +1,29 @@
+# Conditional logical operators
+print (1 == 1) # equal to ==
+print (1 != 1) # not equal to !=
+print (1 > 1) # greater than >
+print (1 < 1) # less than <
+print (1 >= 1) # greater than or equal to >=
+print (1 <= 1) # less than or equal to <=
+print (1 and 1) # and
+print (1 or 1) # or
+print (not 1) # not
+print (1 is 1) # is
+print (1 is not 1) # is not
+print (1 in [1, 2, 3]) # in
+print (1 not in [1, 2, 3]) # not in
+
+# Practical real life examples of conditional logical operators
+print (1 == 1) # True
+print (1 != 1) # False
+print (1 > 1) # False
+print (1 < 1) # False
+print (1 >= 1) # True
+print (1 <= 1) # True
+print (1 and 1) # 1
+print (1 or 1) # 1
+print (not 1) # False
+print (1 is 1) # True
+print (1 is not 1) # False
+print (1 in [1, 2, 3]) # True
+print (1 not in [1, 2, 3]) # False
