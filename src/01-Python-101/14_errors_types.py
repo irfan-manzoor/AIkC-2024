715e9127aa7bf7a8ebd47ddfdba88f497469576c
Irfan Manzoor <rayazl@outlook.com>
2024-03-31 22:11:23 +0500

    errors

---
+++ src/01-Python-101/14_errors_types.py
@@ -0,0 +1,18 @@
+# Syntax error
+x = 10
+if x == 10:
+    print ("hello, world!")
+
+# Runtime error
+x = 10
+y = 0
+print (x / y)
+
+# Semantic error
+name = "Arfan"
+print ("hello, name")
+
+# Logical error
+x = 10
+if x == 10:
+    print ("hello, world!")
